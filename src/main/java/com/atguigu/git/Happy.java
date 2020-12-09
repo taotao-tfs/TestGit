2ffb385844ad34d381121abab8e7d3922cda9164
taotao-tfs <1657585617@qq.com>
2020-12-10 01:35:50 +0800

    tfs commit

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -5,10 +5,7 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("right...");
 		System.out.println("leaf...");
-<<<<<<< HEAD
 		System.out.println("test hotfix...");
-=======
 		System.out.println("Sisi,I love you!");
->>>>>>> refs/heads/sisi
 	}
 }
